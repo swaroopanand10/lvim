5b91e04d3836ebd01d746ee75b454e36fdf4ea5c
swaroopanand <swaroopanand695@gmail.com>
2024-01-07 04:52:28 +0530

    added a keybinding in flash to jump to a line

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -15,6 +15,20 @@ return {
   },
   {
     "folke/flash.nvim",
+    keys = {
+      {
+        "<leader>jk",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump({
+            search = { mode = "search", max_length = 0 },
+            label = { after = { 0, 0 } },
+            pattern = "^",
+          })
+        end,
+        desc = "Label beginning of line",
+      },
+    },
     opts = {
       -- labels = "asdfghjklqwertyuiopzxcvbnm", --default
       labels = "asdfghjkl;uionwertmpqyzxcvb", --customized
