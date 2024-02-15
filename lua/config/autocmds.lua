ffcc91c88aa8a8dcfbc991c9e1d51d988bb2e563
swaroopanand <swaroopanand@email.com>
2024-02-15 15:55:11 +0530

    added temp autocommands for handling the temp error on quiting nvim

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -152,3 +152,15 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     )
   end,
 })
+
+vim.api.nvim_create_autocmd({ "VimLeave" }, {
+  callback = function()
+    vim.cmd('!notify-send  "hello"')
+    vim.cmd('sleep 10m')
+  end,
+})
+vim.api.nvim_create_autocmd({ "VimLeave" }, {
+  callback = function()
+    vim.fn.jobstart('notify-send "hello"', {detach=true})
+  end,
+})
