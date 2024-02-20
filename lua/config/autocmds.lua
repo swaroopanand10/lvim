4b5deb61e6f1b2fd824063e71a662db88c88a998
swaroopanand <swaroopanand@email.com>
2024-02-20 18:11:14 +0530

    disabled image.nvim for now since it was causing the exit error, and thus also removed autocommands for solving exit errors since don't need them now

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -153,14 +153,14 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   end,
 })
 
-vim.api.nvim_create_autocmd({ "VimLeave" }, {
-  callback = function()
-    vim.cmd('!notify-send  "hello"')
-    vim.cmd('sleep 10m')
-  end,
-})
-vim.api.nvim_create_autocmd({ "VimLeave" }, {
-  callback = function()
-    vim.fn.jobstart('notify-send "hello"', {detach=true})
-  end,
-})
+-- vim.api.nvim_create_autocmd({ "VimLeave" }, {
+--   callback = function()
+--     vim.cmd('!notify-send  "hello"')
+--     vim.cmd('sleep 10m')
+--   end,
+-- })
+-- vim.api.nvim_create_autocmd({ "VimLeave" }, {
+--   callback = function()
+--     vim.fn.jobstart('notify-send "hello"', {detach=true})
+--   end,
+-- })
--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,6 +2,6 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   -- { "rcarriga/nvim-notify", enabled = false },
   { "ggandor/flit.nvim", enabled = false },
-  -- { "nvim-lualine/lualine.nvim", enabled = false },
   { "folke/persistence", enabled = false },
+  {"3rd/image.nvim", enabled = false},
 }
